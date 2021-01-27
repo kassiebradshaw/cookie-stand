cedb5941ef598e6274ae818dbaae90af955fc533
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-01-26 22:15:15 -0800

    basic framework completed

---
+++ js/app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+console.log('Hello World');
+
+// get each store element by ID.
+
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+
+let seattle = {
+  name: 'Seattle',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+  randomCust: function() {
+      // method rules go here
+  }
+
+  hourlyCookies: function() {
+      // method rules go here
+  }
+
+  // a method calculate random number of customers per hour (using magic random inclusive)
+  // a method to calculate and populate our number of cookies sold per hour array
+  // a method to render the list items.
+}
