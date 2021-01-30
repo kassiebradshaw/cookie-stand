ade45fdef377a95f38514e2a8b372a58deda9202
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-01-30 14:35:40 -0800

    got constructor function working

--- js/app.js
+++ js/app.js
@@ -1,208 +1,69 @@
 'use strict';
 
-// console.log('Hello World');
-
-// get each store element by ID.
 
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let storeTable = document.getElementById('store-table');
+let stores = ['Seattle', 'Tokyo', 'Dubai', 'Paris', 'Lima'];
+let 
 
-let seattle = {
-  name: 'Seattle',
-  minHourlyCustomer: 23,
-  maxHourlyCustomer: 65,
-  avgCookiesSoldPerCustomer: 6.3,
-  cookiesSoldHourlyArray: [],
-  dailyStoreTotal: 0,
 
-  randomCustomersHourly: function() {
-    return Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
-  },
+// replace all object literals w/ a SINGLE CONSTRUCTOR FUNCTION
 
-  calcCookiesSoldEachHour: function() {
-    for (let i = 0; i < hours.length; i++) {
-      let currentHourCustomers = this.randomCustomersHourly();
-      let cookiesSoldCurrentHour = Math.ceil(currentHourCustomers * this.avgCookiesSoldPerCustomer);
-      this.cookiesSoldHourlyArray.push(cookiesSoldCurrentHour);
-      this.dailyStoreTotal += cookiesSoldCurrentHour;
-      // console.log(`Customers this hour = ${currentHourCustomers}, total cookies this hour = ${cookiesSoldCurrentHour}`);
-      // console.log(this.dailyStoreTotal);
-    }
-  },
+function Store (name, minHourlyCustomer, maxHourlyCustomer, avgCookiesSoldPerCustomer) {
+  this.name = name;
+  this.minHourlyCustomer = minHourlyCustomer;
+  this.maxHourlyCustomer = maxHourlyCustomer;
+  this.avgCookiesSoldPerCustomer = avgCookiesSoldPerCustomer;
+}
 
-  render: function() {
-    this.calcCookiesSoldEachHour();
-    // console.log('I am in the render method');
-    let seattleList = document.getElementById('seattle');
-    // console.log(seattleList + 'proof of life');
-    for (let i=0; i < hours.length; i++) {
-      let li = document.createElement('li');
-      li.textContent = `${hours[i]}: ${this.cookiesSoldHourlyArray[i]}`;
-      seattleList.appendChild(li);
-    }
-    let li = document.createElement('li');
-    li.textContent = `Total: ${this.dailyStoreTotal}`;
-    seattleList.appendChild(li);
-  },
+Store.prototype.randomCustomersHourly = function () {
+  return Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
 };
 
-let tokyo = {
-  name: 'Tokyo',
-  minHourlyCustomer: 3,
-  maxHourlyCustomer: 24,
-  avgCookiesSoldPerCustomer: 1.2,
-  cookiesSoldHourlyArray: [],
-  dailyStoreTotal: 0,
-
-  randomCustomersHourly: function() {
-    return Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
-  },
-
-  calcCookiesSoldEachHour: function() {
-    for (let i = 0; i < hours.length; i++) {
-      let currentHourCustomers = this.randomCustomersHourly();
-      let cookiesSoldCurrentHour = Math.ceil(currentHourCustomers * this.avgCookiesSoldPerCustomer);
-      this.cookiesSoldHourlyArray.push(cookiesSoldCurrentHour);
-      this.dailyStoreTotal += cookiesSoldCurrentHour;
-      // console.log(`Customers this hour = ${currentHourCustomers}, total cookies this hour = ${cookiesSoldCurrentHour}`);
-      // console.log(this.dailyStoreTotal);
-    }
-  },
-
-  render: function() {
-    this.calcCookiesSoldEachHour();
-    // console.log('I am in the render method');
-    let tokyoList = document.getElementById('tokyo');
-    // console.log(tokyoList + 'proof of life');
-    for (let i=0; i < hours.length; i++) {
-      let li = document.createElement('li');
-      li.textContent = `${hours[i]}: ${this.cookiesSoldHourlyArray[i]}`;
-      tokyoList.appendChild(li);
-    }
-    let li = document.createElement('li');
-    li.textContent = `Total: ${this.dailyStoreTotal}`;
-    tokyoList.appendChild(li);
-  },
+Store.prototype.calcCookiesSoldEachHour = function () {
+  for (let i = 0; i < hours.length; i++) {
+    let currentHourCustomers = this.randomCustomersHourly();
+    let cookiesSoldCurrentHour = Math.ceil(currentHourCustomers * this.avgCookiesSoldPerCustomer);
+    this.cookiesSoldHourlyArray.push(cookiesSoldCurrentHour);
+    this.dailyStoreTotal += cookiesSoldCurrentHour;
+    // console.log(`Customers this hour = ${currentHourCustomers}, total cookies this hour = ${cookiesSoldCurrentHour}`);
+    // console.log(this.dailyStoreTotal);
+  }
 };
 
-let dubai = {
-  name: 'Dubai',
-  minHourlyCustomer: 11,
-  maxHourlyCustomer: 38,
-  avgCookiesSoldPerCustomer: 3.7,
-  cookiesSoldHourlyArray: [],
-  dailyStoreTotal: 0,
+// create table row and append to DOM all of the contents below
+Store.prototype.render = function () {
+  this.calcCookiesSoldEachHour();
+  // creates the table row element
+  let tr = document.createElement('tr');
+  // appends the table row to the DOM
+  storeTable.appendChild(tr);
 
-  randomCustomersHourly: function() {
-    return Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
-  },
+  // create table header, give content, and append to the DOM
+  let th = document.createElement('th');
+  th.textContent = this.name;
+  tr.appendChild(th);
 
-  calcCookiesSoldEachHour: function() {
-    for (let i = 0; i < hours.length; i++) {
-      let currentHourCustomers = this.randomCustomersHourly();
-      let cookiesSoldCurrentHour = Math.ceil(currentHourCustomers * this.avgCookiesSoldPerCustomer);
-      this.cookiesSoldHourlyArray.push(cookiesSoldCurrentHour);
-      this.dailyStoreTotal += cookiesSoldCurrentHour;
-      // console.log(`Customers this hour = ${currentHourCustomers}, total cookies this hour = ${cookiesSoldCurrentHour}`);
-      // console.log(this.dailyStoreTotal);
-    }
-  },
-
-  render: function() {
-    this.calcCookiesSoldEachHour();
-    // console.log('I am in the render method');
-    let dubaiList = document.getElementById('dubai');
-    // console.log(dubaiList + 'proof of life');
-    for (let i=0; i < hours.length; i++) {
-      let li = document.createElement('li');
-      li.textContent = `${hours[i]}: ${this.cookiesSoldHourlyArray[i]}`;
-      dubaiList.appendChild(li);
-    }
-    let li = document.createElement('li');
-    li.textContent = `Total: ${this.dailyStoreTotal}`;
-    dubaiList.appendChild(li);
-  },
+  // create multiple table datas, give content, append to DOM
+  for (let i=0; i < hours.length; i++) {
+    let td = document.createElement('td');
+    td.textContent = `${hours[i]}: ${this.cookiesSoldHourlyArray[i]} cookies`;
+    tr.appendChild(td);
+  }
+  let td = document.createElement('td');
+  td.textContent = `Total: ${this.dailyStoreTotal} cookies`;
+  tr.appendChild(td);
 };
 
-let paris = {
-  name: 'Paris',
-  minHourlyCustomer: 20,
-  maxHourlyCustomer: 38,
-  avgCookiesSoldPerCustomer: 2.3,
-  cookiesSoldHourlyArray: [],
-  dailyStoreTotal: 0,
+let seattleStore = new Store ('Seattle', 23, 65, 6.3);
+console.log(`here is my ${seattleStore}`);
+let tokyoStore = new Store ('Tokyo', 3, 25, 1.2);
+console.log(`here is my ${tokyoStore}`);
+let dubaiStore = new Store ('Dubai', 11, 38, 3.7);
+console.log(`here is my ${dubaiStore}`);
+let parisStore = new Store ('Paris', 20, 38, 2.3);
+console.log(`here is my ${parisStore}`);
+let limaStore = new Store ('Lima', 2, 16, 4.6);
+console.log(`here is my ${limaStore}`);
 
-  randomCustomersHourly: function() {
-    return Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
-  },
-
-  calcCookiesSoldEachHour: function() {
-    for (let i = 0; i < hours.length; i++) {
-      let currentHourCustomers = this.randomCustomersHourly();
-      let cookiesSoldCurrentHour = Math.ceil(currentHourCustomers * this.avgCookiesSoldPerCustomer);
-      this.cookiesSoldHourlyArray.push(cookiesSoldCurrentHour);
-      this.dailyStoreTotal += cookiesSoldCurrentHour;
-      // console.log(`Customers this hour = ${currentHourCustomers}, total cookies this hour = ${cookiesSoldCurrentHour}`);
-      // console.log(this.dailyStoreTotal);
-    }
-  },
-
-  render: function() {
-    this.calcCookiesSoldEachHour();
-    // console.log('I am in the render method');
-    let parisList = document.getElementById('paris');
-    // console.log(parisList + 'proof of life');
-    for (let i=0; i < hours.length; i++) {
-      let li = document.createElement('li');
-      li.textContent = `${hours[i]}: ${this.cookiesSoldHourlyArray[i]}`;
-      parisList.appendChild(li);
-    }
-    let li = document.createElement('li');
-    li.textContent = `Total: ${this.dailyStoreTotal}`;
-    parisList.appendChild(li);
-  },
-};
-
-let lima = {
-  name: 'Lima',
-  minHourlyCustomer: 2,
-  maxHourlyCustomer: 16,
-  avgCookiesSoldPerCustomer: 4.6,
-  cookiesSoldHourlyArray: [],
-  dailyStoreTotal: 0,
-
-  randomCustomersHourly: function() {
-    return Math.floor(Math.random() * (this.maxHourlyCustomer - this.minHourlyCustomer + 1) + this.minHourlyCustomer);
-  },
-
-  calcCookiesSoldEachHour: function() {
-    for (let i = 0; i < hours.length; i++) {
-      let currentHourCustomers = this.randomCustomersHourly();
-      let cookiesSoldCurrentHour = Math.ceil(currentHourCustomers * this.avgCookiesSoldPerCustomer);
-      this.cookiesSoldHourlyArray.push(cookiesSoldCurrentHour);
-      this.dailyStoreTotal += cookiesSoldCurrentHour;
-      // console.log(`Customers this hour = ${currentHourCustomers}, total cookies this hour = ${cookiesSoldCurrentHour}`);
-      // console.log(this.dailyStoreTotal);
-    }
-  },
-
-  render: function() {
-    this.calcCookiesSoldEachHour();
-    // console.log('I am in the render method');
-    let limaList = document.getElementById('lima');
-    // console.log(limaList + 'proof of life');
-    for (let i=0; i < hours.length; i++) {
-      let li = document.createElement('li');
-      li.textContent = `${hours[i]}: ${this.cookiesSoldHourlyArray[i]}`;
-      limaList.appendChild(li);
-    }
-    let li = document.createElement('li');
-    li.textContent = `Total: ${this.dailyStoreTotal}`;
-    limaList.appendChild(li);
-  },
-};
-
-seattle.render();
-tokyo.render();
-dubai.render();
-paris.render();
-lima.render();
+seattleStore.render();
